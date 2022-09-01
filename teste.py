211fd61e66e0488afe4ff59e6ad68d7718f45941
vfreis <vinicios.falqueiro@gmail.com>
2022-09-01 17:11:31 -0300

    insert parcialmente funcionando; nao substitue os valores de entrada de insert_cliente

--- teste.py
+++ teste.py
@@ -1,7 +1,5 @@
 # teste library
-import pyodbc 
-import pandas as pd
-
+import pyodbc
 # engine = create_engine('sqlite:///clientes.db', echo = True)
 
 ## infos do banco
@@ -12,16 +10,38 @@ username = 'admin'
 password = '123456789'
 
 # setup connection
-try:
-    cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
-    cursor = cnxn.cursor()
-    print('conectado com ' + database)
-except: 
-        print('não foi possivel conectar')
+cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+cursor = cnxn.cursor()
+print('conectado com ' + database)
 
 
-def select_basico():
-    sql = "SELECT * FROM [clientes] "
+#metodos para aplicação
+def select_cliente_id(id):
+    try:
+        sql = "SELECT * FROM [clientes] WHERE ID = " + str(id)
+        cursor.execute(sql)
+        for r in cursor:
+            print(r) # para teste apenas
+            return(r)
+    except:
+        return('N/D')
+
+def select_todos_clientes():
+    sql = "SELECT * FROM [clientes]"
     cursor.execute(sql)
     for r in cursor:
-        print(r)
+        return(r)
+
+
+
+def insert_cliente(status, nome, dt_nasc, email, celular, telefone, sexo, cpf):
+    # sql = f'''insert into clientes (status, nome, dt_nasc, email, celular, telefone, sexo, cpf) values({status} , {nome}, {dt_nasc}, {email}, {celular}, {telefone}, {sexo}, {cpf} )'''
+    sql = '''insert into clientes (status, nome, dt_nasc, email, celular, telefone, sexo, cpf) values( 'true', 'william', '01/01/1989', 'willdavila@bla.com.br', '1199999999', '1100000000', '?', '0000000001')'''
+    cursor.execute(sql)
+    cnxn.commit()
+    print(f'cliente {nome} adicionado com sucesso')
+
+insert_cliente('true', 'william2', '01/01/1989', 'willdavila@bla.com.br'," 1199999999", "1100000000", '?', "0000000001")
+
+# insert_teste = '''insert into clientes (status, nome, dt_nasc, email, celular, telefone, sexo, cpf) values( 'true', 'william', '01/01/1989', 'willdavila@bla.com.br', '1199999999', '1100000000', '?', '0000000001')'''
+
