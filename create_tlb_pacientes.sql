43e0b87dd4c5eefd4ccad0d6629f827abd8a4ca3
vfreis <vinicios.falqueiro@gmail.com>
2022-09-02 18:52:34 -0300

    alterado tabela clientes para pacientes; cpf agora é pk; consulta por cpf adicionada; conversao dict to json realizada;

--- create_tlb_pacientes.sql
+++ create_tlb_pacientes.sql
@@ -1,14 +1,14 @@
-create table clientes (
+create table pacientes (
 
-	id int primary key identity,
+	id int identity,
 	status bit not null, -- verdadeiro sempre
 	nome varchar(50) not null,
-	dt_nasc datetime not null,
+	dt_nasc date not null,
 	email varchar(50) not null,
 	celular varchar(50) not null,
 	telefone varchar(50),
 	sexo varchar(50) not null,
-	cpf varchar(50) not null,
-	data_criacao timestamp,
+	cpf varchar(50) primary key not null,
+	data_criacao date,
 
 )
--- teste.py
+++ teste.py
@@ -1,50 +1,77 @@
-# teste library
 import pyodbc
-import jsonify
-# engine = create_engine('sqlite:///clientes.db', echo = True)
 
-## infos do banco
-server = 'consultorio-app-alpha.ciofokjqok2t.us-east-1.rds.amazonaws.com'
-porta = 1433
-database = 'consulta_app_alpha'
-username = 'admin'
-password = '123456789'
+# para conversão e visualização de consultas
+import json
+
+from datetime import date
+
+
+#variaveis de ambiente
+from env_var import env_var
+
+## variaveis de ambiente
+server = env_var['server']
+porta = env_var['porta']
+database = env_var['database']
+username = env_var['username']
+password = env_var['password']
 
 # setup connection
 cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
 cursor = cnxn.cursor()
 print('conectado com ' + database)
 
-
 #metodos para aplicação
-def select_cliente_id(id):
-    try:
-        sql = "SELECT * FROM [clientes] WHERE ID = " + str(id)
-        cursor.execute(sql)
-        for r in cursor:
-            print(r) # para teste apenas
-            return jsonify(r)
-    except:
-        return('N/D')
+def paciente_por_id(id):
+    query = cursor.execute(f"SELECT * FROM pacientes WHERE id = {id}")
+    columns = [column[0] for column in cursor.description]
+    results = []
+    for row in cursor.fetchall(): 
+        results.append(dict(zip(columns, row)))
+    jsonobj = json.dumps(results, indent=4, sort_keys=False)
+    print(jsonobj) # apenas para teste
+    return jsonobj
 
-def select_todos_clientes():
-    cursor.execute("SELECT * FROM CLIENTES;") 
-    row = cursor.fetchone() 
-    while row: 
-        row = cursor.fetchone()
-        print(row)
+def paciente_por_cpf(cpf):
+    query = cursor.execute(f"SELECT * FROM pacientes WHERE CPF = {cpf}")
+    columns = [column[0] for column in cursor.description]
+    results = []
+    for row in cursor.fetchall(): 
+        results.append(dict(zip(columns, row)))
+    jsonobj = json.dumps(results, indent=4, sort_keys=True, default=str)
+    print(jsonobj) # apenas para teste
+    return jsonobj
 
+def select_todos_pacientes():
+    query = cursor.execute("SELECT * FROM pacientes WHERE status = 'True'")
+    columns = [column[0] for column in cursor.description]
+    results = []
+    for row in cursor.fetchall():
+        results.append(dict(zip(columns, row)))
+    jsonobj = json.dumps(results, indent=4, sort_keys=True, default=str)
+    print(jsonobj) # apenas para teste
+    return jsonobj
 
-
-def insert_cliente(status, nome, dt_nasc, email, celular, telefone, sexo, cpf):
-    sql = f'''insert into clientes (status, nome, dt_nasc, email, celular, telefone, sexo, cpf) values('{status}' , '{nome}', '{dt_nasc}', '{email}', '{celular}', '{telefone}', '{sexo}', '{cpf}' )'''
-    # sql = '''insert into clientes (status, nome, dt_nasc, email, celular, telefone, sexo, cpf) values( 'true', 'william', '01/01/1989', 'willdavila@bla.com.br', '1199999999', '1100000000', '?', '0000000001')'''
+def inserir_paciente(status, nome, dt_nasc, email, celular, telefone, sexo, cpf):
+    timestamp = date.today()
+    sql = f'''insert into pacientes (status, nome, dt_nasc, email, celular, telefone, sexo, cpf, data_criacao) values('{status}' , '{nome}', '{dt_nasc}', '{email}', '{celular}', '{telefone}', '{sexo}', '{cpf}', '{timestamp}')'''
     cursor.execute(sql)
     cnxn.commit()
     print(f'cliente {nome} adicionado com sucesso')
+    print(paciente_por_id(id))
 
-# insert_cliente('true', 'william3', '01/01/1989', 'willdavila@bla.com.br'," 1199999999", "1100000000", '?', "0000000001") # teste
+def deletar_paciente(id):
+    sql = f'''UPDATE pacientes SET Status = 'False' WHERE ID =  {id}'''
+    cursor.execute(sql)
+    cnxn.commit()
+    print(paciente_por_id(id))
 
-select_todos_clientes() # seleciona todos os clientes
+# insert_cliente('true', 'tiuzin', '24/24/1989', 'willdavila@bla.com.br', " 1199999999", "1100000000", '?', "0000000001") # não sabemos pq nao funciona
 
-select_cliente_id(1)
+# inserir_paciente('true', 'tiuzin', '01/01/1989', 'willdavila@bla.com.br'," 1199999999", "1100000000", '?', "0000000001")
+
+# select_todos_clientes() # seleciona todos os clientes
+
+# delete_cliente(4)
+
+paciente_por_id(1)
