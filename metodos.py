273c1260ed61b6c087fc68527b7b88414013bd72
vfreis <vinicios.falqueiro@gmail.com>
2022-09-02 22:58:49 -0300

    criando api

--- metodos.py
+++ metodos.py
@@ -21,64 +21,69 @@ cursor = cnxn.cursor()
 print('conectado com ' + database)
 
 #metodos para aplicação
-def paciente_por_id(id):
-    query = cursor.execute(f"SELECT * FROM pacientes WHERE id = {id}")
-    columns = [column[0] for column in cursor.description]
-    results = []
-    for row in cursor.fetchall(): 
-        results.append(dict(zip(columns, row)))
-    jsonstr = json.dumps(results, indent=4, sort_keys=False)
-    jsonobj = json.loads(jsonstr)
-    # print(jsonobj) # apenas para teste
-    return jsonobj
 
-def paciente_por_cpf(cpf):
-    query = cursor.execute(f"SELECT * FROM pacientes WHERE CPF = {cpf}")
-    columns = [column[0] for column in cursor.description]
-    results = []
-    for row in cursor.fetchall(): 
-        results.append(dict(zip(columns, row)))
-    jsonstr = json.dumps(results, indent=4, sort_keys=False)
-    jsonobj = json.loads(jsonstr)
-    # print(jsonobj) # apenas para teste
-    return jsonobj
+class Metodos:
 
-def selecionar_todos_pacientes():
-    query = cursor.execute(f"SELECT * FROM pacientes WHERE STATUS = 'True'")
-    columns = [column[0] for column in cursor.description]
-    results = []
-    for row in cursor.fetchall(): 
-        results.append(dict(zip(columns, row)))
-    jsonstr = json.dumps(results, indent=4, sort_keys=False)
-    jsonobj = json.loads(jsonstr)
-    # print(jsonobj) # apenas para teste
-    return jsonobj
+    def __init__(self, n):
+        self.n = n       
 
-def inserir_paciente(status, nome, dt_nasc, email, celular, telefone, sexo, cpf):
-    timestamp = date.today()
-    sql = f'''insert into pacientes (status, nome, dt_nasc, email, celular, telefone, sexo, cpf, data_criacao) values('{status}' , '{nome}', '{dt_nasc}', '{email}', '{celular}', '{telefone}', '{sexo}', '{cpf}', '{timestamp}')'''
-    cursor.execute(sql)
-    cnxn.commit()
-    temp = paciente_por_cpf(cpf)
-    print(f'cliente {nome} adicionado com sucesso')
-    return(paciente_por_cpf(cpf))
+    def paciente_por_id(id):
+        query = cursor.execute(f"SELECT * FROM pacientes WHERE id = {id}")
+        columns = [column[0] for column in cursor.description]
+        results = []
+        for row in cursor.fetchall(): 
+            results.append(dict(zip(columns, row)))
+        jsonstr = json.dumps(results, indent=4, sort_keys=False)
+        jsonobj = json.loads(jsonstr)
+        # print(jsonobj) # apenas para teste
+        return jsonobj
 
-def deletar_paciente(cpf):
-    sql = f'''UPDATE pacientes SET Status = 'False' WHERE CPF =  {cpf}'''
-    cursor.execute(sql)
-    cnxn.commit()
-    return(paciente_por_cpf(cpf))
+    def paciente_por_cpf(cpf):
+        query = cursor.execute(f"SELECT * FROM pacientes WHERE CPF = {cpf}")
+        columns = [column[0] for column in cursor.description]
+        results = []
+        for row in cursor.fetchall(): 
+            results.append(dict(zip(columns, row)))
+        jsonstr = json.dumps(results, indent=4, sort_keys=False)
+        jsonobj = json.loads(jsonstr)
+        # print(jsonobj) # apenas para teste
+        return jsonobj
 
-def altera_paciente(campo, alteracao, cpf):
-    sql = f'''UPDATE pacientes SET {campo} = '{alteracao}' WHERE CPF =  {cpf}'''
-    cursor.execute(sql)
-    cnxn.commit()
-    return(paciente_por_cpf(cpf))
+    def selecionar_todos_pacientes():
+        query = cursor.execute(f"SELECT * FROM pacientes WHERE STATUS = 'True'")
+        columns = [column[0] for column in cursor.description]
+        results = []
+        for row in cursor.fetchall(): 
+            results.append(dict(zip(columns, row)))
+        jsonstr = json.dumps(results, indent=4, sort_keys=False)
+        jsonobj = json.loads(jsonstr)
+        # print(jsonobj) # apenas para teste
+        return jsonobj
 
-print(altera_paciente('nome', 'vira vira vira teu', '0000000005'))
+    def inserir_paciente(nome, dt_nasc, email, celular, telefone, sexo, cpf):
+        timestamp = date.today()
+        sql = f'''insert into pacientes (status, nome, dt_nasc, email, celular, telefone, sexo, cpf, data_criacao) values('True' , '{nome}', '{dt_nasc}', '{email}', '{celular}', '{telefone}', '{sexo}', '{cpf}', '{timestamp}')'''
+        cursor.execute(sql)
+        cnxn.commit()
+        temp = Metodos.paciente_por_cpf(cpf)[0]['nome']
+        print(f'paciente {temp} adicionado com sucesso')
+        return(Metodos.paciente_por_cpf(cpf))
 
+    def deletar_paciente(cpf):
+        sql = f'''UPDATE pacientes SET Status = 'False' WHERE CPF =  {cpf}'''
+        cursor.execute(sql)
+        cnxn.commit()
+        return(Metodos.paciente_por_cpf(cpf))
 
-# inserir_paciente('true', 'danial do mario', '01/01/1989', 'willdavila@bla.com.br'," 1199999999", "1100000000", '?', "0000000005")
+    def altera_paciente(campo, alteracao, cpf):
+        sql = f'''UPDATE pacientes SET {campo} = '{alteracao}' WHERE CPF =  {cpf}'''
+        cursor.execute(sql)
+        cnxn.commit()
+        return(Metodos.paciente_por_cpf(cpf))
+
+# print(altera_paciente('nome', 'edher', '0000000004'))
+
+# inserir_paciente('debouas', '01/01/1989', 'gabidavila@bla.com.br'," 1199999999", "1100000000", 'f', "0000000008")
 
 # print(selecionar_todos_pacientes()) # seleciona todos os clientes
 
--- servidor.py
+++ servidor.py
@@ -1,1 +1,19 @@
 # library
+from ast import Import
+import os
+from flask import Flask, request, jsonify, render_template, url_for
+import pyodbc
+# metodos
+from metodos import Metodos
+
+app = Flask(__name__)
+
+@app.route('/')
+def init():
+    return render_template('index.html')
+
+
+# print(Metodos.paciente_por_cpf('0000000008'))
+# print(Metodos.inserir_paciente('kenshin', '01/01/1989', 'gabidavila@bla.com.br'," 1199999999", "1100000000", 'f', "0000000010"))
+
+
