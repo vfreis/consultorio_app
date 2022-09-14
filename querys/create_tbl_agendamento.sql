bbbf08866488c0591993f688ccd302ceac8fd379
Vinicios Falqueiro Reis <vinicios.falqueiro@gmail.com>
2022-09-14 20:44:40 -0300

    funcoes em mysql funcionando

--- querys/create_tbl_agendamento.sql
+++ querys/create_tbl_agendamento.sql
@@ -3,18 +3,16 @@ use consulta_app_alpha
 drop table if exists agendamentos
 
 create table agendamentos (
-
-	id int identity,
-	status bit not null, -- verdadeiro sempre
-    cpf_paciente varchar(50) not null,
+	id int AUTO_INCREMENT PRIMARY key,
+	status bool not NULL, 
+   cpf_paciente varchar(50) not null,
 	paciente varchar(50) not null,
 	data_consulta date not null,
-    hora_consulta time not null,
-    consultorio varchar(50) not null,
-    medico varchar(50) not null,
-    --cpf_medico varchar(50)
-    especialidade varchar(50) not null,
-    confirma bit null, 
-    chave varchar(50) 
-	
+   hora_consulta time not null,
+   consultorio varchar(50) not null,
+   medico varchar(50)  ,
+   #cpf_medico varchar(50)
+   especialidade varchar(50) not null,
+   confirma bit null, 
+   chave varchar(50) 
         )
