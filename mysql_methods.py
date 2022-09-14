d2fa9938e47ed2b2e9dd1e5a4e7ec27764a5e3d7
Vinicios Falqueiro Reis <vinicios.falqueiro@gmail.com>
2022-09-14 19:54:48 -0300

    adicionar usuario e realizar consulta funcionando

--- mysql_methods.py
+++ mysql_methods.py
@@ -33,40 +33,42 @@ class Conexao_DB():
         json_str = json.dumps(resultados, indent=4, sort_keys=False)
         json_obj = json.loads(json_str)
         return json_obj
-    
+
+
 
     def realizar_consulta(consulta):
             conn = mysql.connect()
             cursor = conn.cursor()
             cursor.execute(consulta)
-            resultado = cursor.fetchall()
-            resultado_dic = list(resultado)
-            resultado_json = str(resultado_dic)
-            rf = json.dumps(resultado_json)
-            rf_json = json.loads(rf)
-            return type(rf_json)
+            resultados =[]
+            colunas = [coluna[0] for coluna in cursor.description]
+            for linha in cursor.fetchall():
+                resultados.append(dict(zip(colunas, linha)))
+            return resultados
+            
 
 
-    def inserir_agente(nome, dt_nasc, email, celular, telefone, sexo, cpf, senha, agente):
+    def inserir_agente(nome, dt_nasc, email, celular, telefone, sexo, cpf, senha, agente = 'pacientes'):
         conn = mysql.connect()
         cursor = conn.cursor()
         timestamp = date.today()
         insert = f'''insert into {agente} (status, nome, dt_nasc, email, celular, telefone, sexo, cpf, senha, data_criacao) values('True' , '{nome}', '{dt_nasc}', '{email}', '{celular}', '{telefone}', '{sexo}', '{cpf}','{senha}', '{timestamp}')'''
         cursor.execute(insert)
         conn.commit()
-        return nome + ' adicionado'
+        return Conexao_DB.selecionar_agente_cpf(cpf, agente)[0]['nome'] + ' adicionado'
 
-    # def selecionar_agente_cpf(cpf, agente = 'pacientes'):
-    #     consulta = f'''SELECT * FROM {agente} WHERE CPF = {cpf}'''
-    #     resultado = Conexao_DB.realizar_consulta(consulta)
-    #     return resultado
+    def selecionar_agente_cpf(cpf, agente = 'pacientes'):
+        consulta = f'''SELECT * FROM {agente} WHERE CPF = {cpf}'''
+        resultado = Conexao_DB.realizar_consulta(consulta)
+        return resultado
 
-consulta = '''select * from pacientes'''
-print(Conexao_DB.realizar_consulta(consulta))
 
+
+# consulta = '''select * from pacientes'''
+# print(Conexao_DB.realizar_consulta(consulta))
 # consulta = ''' insert into pacientes (status, nome, dt_nasc, email, celular, telefone, sexo, cpf, senha, data_criacao ) values (true,'Edherzito', '01-01-1929', 'edherzito@blaaa.com.br',"1195866325", "11495464987", 'm', "000005900", "senha", curdate()) '''
 # print(Conexao_DB.realizar_consulta(consulta))
-# print(Conexao_DB.inserir_agente('Gabi', '01/01/1929', 'gabi@blaa.com',"1195866325", "11495464989", 'm', "0000059812", "senha", 'pacientes'))
+print(Conexao_DB.inserir_agente('Teste', '01/01/1929', 'teste',"1195866325", "11495464989", 'm', "11111111111", "senha", 'pacientes'))
 #consulta = 'select * from pacientes'
 #print(Conexao_DB.realizar_consulta(consulta))
 #print(Conexao_DB.test_conn())
